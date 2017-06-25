04f3a80e79e99504e0f72cb8f95a1b5f143cbbcf
Trim21 <trim21me@gmail.com>
2017-06-26 03:52:43 +0800

    feat(session): session can auto refresh

--- sdu_bkjws/__init__.py
+++ sdu_bkjws/__init__.py
@@ -1,32 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
 
 class SduBkjws(object):
     def __init__(self, student_id: str, password: str):
         self.student_id = student_id
         self.password = password
-        s = self.login(student_id, password)
-        if s:
+        self.login()
+
+    # return a requests session, which include cookies. you can use it to get
+    # html directly
+    def login(self):
+        self.last_connect = time.time()
+        s = requests.session()
+        s.get('http://bkjws.sdu.edu.cn')
+        data = {
+            'j_username': self.student_id,
+            'j_password': self.password
+        }
+        r6 = s.post('http://bkjws.sdu.edu.cn/b/ajaxLogin', data=data)
+        if r6.text == '"success"':
             self.session = s
         else:
             raise Exception('username or password error')
 
-    # return a requests session, which include cookies. you can use it to get html directly
-    def login(self, student_id: str, password: str):
-        s = requests.session()
-        s.get('http://bkjws.sdu.edu.cn')
-        data = {
-            'j_username': student_id,
-            'j_password': password
-        }
-        r6 = s.post('http://bkjws.sdu.edu.cn/b/ajaxLogin', data=data)
-        if r6.text == '"success"':
-            return s
-        else:
-            return False
+    def keep_live(fn):
+        def wrapper(self, *args, **kwargs):
+            if time.time() - self.last_connect > 15 * 60:
+                self.session = self.login()
+                self.last_connect = time.time()
+            return fn(self, *args, **kwargs)
+        return wrapper
 
     # use session to get lesson html
+    @keep_live
     def get_lesson_html(self):
         s = self.session
         if s:
@@ -45,6 +53,7 @@ class SduBkjws(object):
 
     def get_lesson(self):
         html = self.get_lesson_html()
+        # print(html)
         soup = BeautifulSoup(html, "html.parser")
         s = soup.find('table',
                       attrs={"class": "table table-striped table-bordered table-hover",
