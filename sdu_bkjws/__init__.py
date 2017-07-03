30ed5e83dae55fbe5634ecb1b3f1fe8d32337703
Trim21 <trim21me@gmail.com>
2017-07-03 17:48:26 +0800

    feat(comment): new api to get all lesson to be commented
    get_comment_lesson_info()

--- sdu_bkjws/__init__.py
+++ sdu_bkjws/__init__.py
@@ -4,6 +4,7 @@ import requests
 import time
 from urllib.parse import urlencode
 import random
+import multidict
 
 
 def _keep_live(fn):
@@ -21,6 +22,19 @@ class SduBkjws(object):
         self.student_id = student_id
         self.password = password
         self.session = self.login()
+
+    def aoData(self, echo, columns):
+        aoData = [{"name": "sEcho", "value": echo},
+                  {"name": "iColumns", "value": len(columns)},
+                  {"name": "sColumns", "value": ""},
+                  {"name": "iDisplayStart", "value": 0},
+                  {"name": "iDisplayLength", "value": -1},
+                  ]
+        for index, value in enumerate(columns):
+            aoData.append({"name": "mDataProp_{}".format(index), "value": value})
+            aoData.append({"name": "bSortable_{}".format(index), "value": False})
+
+        return urlencode({"aoData": aoData})
 
     # return a requests session, which include cookies. you can use it to get
     # html directly
@@ -87,9 +101,9 @@ class SduBkjws(object):
         :type: dict
         :return:
         """
-        r = self.session.post("http://bkjws.sdu.edu.cn/b/grxx/xs/xjxx/detail",
-                              headers={"X-Requested-With": "XMLHttpRequest"})
-        r = r.json()
+        r = self.post("http://bkjws.sdu.edu.cn/b/grxx/xs/xjxx/detail",
+                      headers={"X-Requested-With": "XMLHttpRequest"}, data=None)
+        # r = r.json()
         if r['result'] == 'success':
             self._detail = r['object']
             return self._detail
@@ -103,28 +117,14 @@ class SduBkjws(object):
         :return:
         """
         echo = random.randint(1, 9)
-        data = {"aoData": [{"name": "sEcho", "value": echo}, {"name": "iColumns", "value": 10},
-                           {"name": "sColumns", "value": ""}, {"name": "iDisplayStart", "value": 0},
-                           {"name": "mDataProp_0", "value": "xnxq"}, {"name": "mDataProp_1", "value": "kch"},
-                           {"name": "mDataProp_2", "value": "kcm"}, {"name": "mDataProp_3", "value": "kxh"},
-                           {"name": "mDataProp_4", "value": "xf"}, {"name": "mDataProp_5", "value": "kssj"},
-                           {"name": "mDataProp_6", "value": "kscjView"},
-                           {"name": "mDataProp_7", "value": "wfzjd"},
-                           {"name": "mDataProp_8", "value": "wfzdj"}, {"name": "mDataProp_9", "value": "kcsx"},
-                           {"name": "iSortCol_0", "value": 5}, {"name": "sSortDir_0", "value": "desc"},
-                           {"name": "iSortingCols", "value": 1}, {"name": "bSortable_0", "value": False},
-                           {"name": "bSortable_1", "value": False}, {"name": "bSortable_2", "value": False},
-                           {"name": "bSortable_3", "value": False}, {"name": "bSortable_4", "value": False},
-                           {"name": "bSortable_5", "value": True}, {"name": "bSortable_6", "value": False},
-                           {"name": "bSortable_7", "value": False}, {"name": "bSortable_8", "value": False},
-                           {"name": "bSortable_9", "value": False}]}
 
-        string = urlencode(data)
-        response = self.session.post("http://bkjws.sdu.edu.cn/b/cj/cjcx/xs/lscx",
-                                     headers={"X-Requested-With": "XMLHttpRequest",
-                                              "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
-                                     data=string)
-        response = response.json()
+        response = self.post("http://bkjws.sdu.edu.cn/b/cj/cjcx/xs/lscx",
+                             headers={"X-Requested-With": "XMLHttpRequest",
+                                      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
+                             data=self.aoData(echo,
+                                              columns=["xnxq", "kcm", "kxh", "xf", "kssj", "kscjView", "wfzjd", "wfzdj",
+                                                       "kcsx"]))
+        # response = response.json()
         if response['result'] == 'success' and response['object']['sEcho'] == str(echo):
             self._raw_past_score = response
             return self._raw_past_score
@@ -150,27 +150,13 @@ class SduBkjws(object):
         :return:
         """
         echo = random.randint(1, 9)
-        data = {"aoData": [{"name": "sEcho", "value": echo}, {"name": "iColumns", "value": 10},
-                           {"name": "sColumns", "value": ""}, {"name": "iDisplayStart", "value": 0},
-                           {"name": "mDataProp_0", "value": "xnxq"}, {"name": "mDataProp_1", "value": "kch"},
-                           {"name": "mDataProp_2", "value": "kcm"}, {"name": "mDataProp_3", "value": "kxh"},
-                           {"name": "mDataProp_4", "value": "xf"}, {"name": "mDataProp_5", "value": "kssj"},
-                           {"name": "mDataProp_6", "value": "kscjView"},
-                           {"name": "mDataProp_7", "value": "wfzjd"},
-                           {"name": "mDataProp_8", "value": "wfzdj"}, {"name": "mDataProp_9", "value": "kcsx"},
-                           {"name": "iSortCol_0", "value": 5}, {"name": "sSortDir_0", "value": "desc"},
-                           {"name": "iSortingCols", "value": 1}, {"name": "bSortable_0", "value": False},
-                           {"name": "bSortable_1", "value": False}, {"name": "bSortable_2", "value": False},
-                           {"name": "bSortable_3", "value": False}, {"name": "bSortable_4", "value": False},
-                           {"name": "bSortable_5", "value": True}, {"name": "bSortable_6", "value": False},
-                           {"name": "bSortable_7", "value": False}, {"name": "bSortable_8", "value": False},
-                           {"name": "bSortable_9", "value": False}]}
-        string = urlencode(data)
-        response = self.session.post("http://bkjws.sdu.edu.cn/b/cj/cjcx/xs/list",
-                                     headers={"X-Requested-With": "XMLHttpRequest",
-                                              "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
-                                     data=string)
-        response = response.json()
+
+        response = self.post("http://bkjws.sdu.edu.cn/b/cj/cjcx/xs/list",
+                             headers={"X-Requested-With": "XMLHttpRequest",
+                                      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
+                             data=self.aoData(echo,
+                                              columns=["xnxq", "kcm", "kxh", "xf", "kssj", "kscjView", "wfzjd", "wfzdj",
+                                                       "kcsx"]))
         if self._check_response(response, echo):
             self._raw_now_score = response
             return self._raw_now_score
@@ -210,36 +196,31 @@ class SduBkjws(object):
         for key, value in info.items():
             if value == None:
                 info[key] = ''
-        r = self.session.post('http://bkjws.sdu.edu.cn/b/grxx/xs/xjxx/save',
-                              headers={'X-Requested-With': 'XMLHttpRequest'},
-                              data=info)
-        r = r.json()
+        r = self.post('http://bkjws.sdu.edu.cn/b/grxx/xs/xjxx/save',
+                      headers={'X-Requested-With': 'XMLHttpRequest'},
+                      data=info)
         if r['result'] == 'success' and r['msg'] == "保存成功":
             return True
         else:
             raise Exception(r, 'unexpected error please create a issue on GitHub')
 
     @_keep_live
-    def get_rank_with_query(self, lesson_num_long, lesson_num, exam_time):
-        kch_kxh_kssj = '{}_{}_{}'.format(lesson_num_long, lesson_num, exam_time)
-        query = {'aoData': '',
-                 'dataTableId_length': -1,
-                 'kch_kxh_kssj': kch_kxh_kssj}
-        print(query)
-        r = self.session.post('http://bkjws.sdu.edu.cn/f/cj/cjcx/xs/xspm',
-                              headers={"X-Requested-With": "XMLHttpRequest",
-                                       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
-                              data=query)
+    def get_multi_rank_with_query(self, search_list):
+        query = "aoData=&dataTableId_length=-1"
+        for obj in search_list:
+            lesson_num_long, lesson_num, exam_time = obj['lesson_num_long'], obj['lesson_num'], obj['exam_time']
+            query += '&kch_kxh_kssj={}_{}_{}'.format(lesson_num_long, lesson_num, exam_time)
+        r = self.post('http://bkjws.sdu.edu.cn/f/cj/cjcx/xs/xspm',
+                      headers={"X-Requested-With": "XMLHttpRequest",
+                               "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
+                      data=query)
         soup = BeautifulSoup(r.text, 'html.parser')
         s = soup.find('table', id='dataTableId')
-        # print(s)
 
         l = s.find_all('tr')
         head = l[0]
         body = l[1:]
         head = list(map(lambda x: x.text, head.find_all('th')))
-        # body = list(map(lambda x: x.find_all('td'), body))
-        print(head)
         body = list(map(lambda x: x.find_all('td'), body))
         objList = []
         for line in body:
@@ -257,3 +238,66 @@ class SduBkjws(object):
                 "min_score": line[head.index('最低分')]
             })
         return objList
+
+    @_keep_live
+    def post(self, url, data, headers=None):
+        if not headers:
+            r = self.session.post(url, headers=headers,
+                                  data=data)
+        else:
+            r = self.session.post(url, headers={"X-Requested-With": "XMLHttpRequest",
+                                                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
+                                  data=data)
+        if r.headers['content-type'].find('json') != -1:
+            return r.json()
+        if r.headers['content-type'].find('html') != -1:
+            return r.text
+
+    @_keep_live
+    def get_rank_with_query(self, lesson_num_long, lesson_num, exam_time):
+        query = "aoData=&dataTableId_length=-1"
+        query += '&kch_kxh_kssj={}_{}_{}'.format(lesson_num_long, lesson_num, exam_time)
+        r = self.post('http://bkjws.sdu.edu.cn/f/cj/cjcx/xs/xspm',
+                      headers={"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                               "X-Requested-With": "XMLHttpRequest"
+                               }, data=query)
+        soup = BeautifulSoup(r, 'html.parser')
+        s = soup.find('table', id='dataTableId')
+        l = s.find_all('tr')
+        head = l[0]
+        body = l[1:]
+        head = list(map(lambda x: x.text, head.find_all('th')))
+        body = list(map(lambda x: x.find_all('td'), body))
+        objList = []
+        for line in body:
+            line = list(map(lambda x: x.text, line))
+            objList.append({
+                "lesson_num_long": line[head.index('课程号')],
+                "lesson_name": line[head.index('课程名')],
+                "lesson_num": line[head.index('课序号')],
+                "credit": line[head.index('学分')],
+                "exam_time": line[head.index('考试时间')],
+                "score": line[head.index('成绩')],
+                "number": line[head.index('选课人数')],
+                "rank": line[head.index('排名')],
+                "max_score": line[head.index('最高分')],
+                "min_score": line[head.index('最低分')]
+            })
+        return objList
+
+    def comment_lesson(self):
+        # todo: this function
+        pass
+
+    @_keep_live
+    def get_comment_lesson_info(self):  # 获取课程序列
+
+        echo = random.randint(0, 9)
+        response = self.post('http://bkjws.sdu.edu.cn/b/pg/xs/list',
+                             headers={"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                                      "X-Requested-With": "XMLHttpRequest"
+                                      }, data=self.aoData(echo, ['kch', 'kcm', 'jsm', 'function', 'function']), )
+        if self._check_response(response, echo):
+            return response['object']['aaData']
+        else:
+            raise Exception(response, 'unexpected error please create a issue on GitHub')
